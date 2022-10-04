027e6d255e8172d863f9177ad167bb1482636aeb
Federico Orsi <federicoantonio.orsi@gmail.com>
2022-10-04 19:11:33 -0300

    ref

--- code.js
+++ code.js
@@ -1,10 +1,41 @@
-// let hello;
+// Calculador de Tiempo restante en MiliSegundos
+const calcularTiempoRestante = (deadLine) =>{
 
-// hello = document.getElementById("helloo");
-// hello.onmouseover = () => {
-//     hello.style.background = "yellow";
-// }
+    let now = new Date ();
+    tiempoRestante = (new Date (deadLine) - now + 1000)/1000;
+    segundosRestantes = ('0' + Math.floor(tiempoRestante % 60)).slice(-2);
+    minutosRestantes = ('0' + Math.floor(tiempoRestante / 60 % 60)).slice(-2);
+    horasRestantes = ('0' + Math.floor(tiempoRestante / 3600 % 24)).slice(-2);
+    diasRestantes = Math.floor(tiempoRestante / 86400);
 
-// hello.onmouseout = () => {
-//     hello.style.background = "aqua";
-// }
+return {
+tiempoRestante, 
+segundosRestantes,
+minutosRestantes,
+horasRestantes,
+diasRestantes,
+}
+
+}
+
+console.log(calcularTiempoRestante('Nov 05 2022 18:00:00 GMT-0300'));
+
+// Mostrar CountDown en Pantalla
+let countdownContainer = document.getElementById("countdownContainer");
+
+const showCountDown = (deadLine) => {
+
+const timer = setInterval(() => {
+tiempo = calcularTiempoRestante(deadLine);
+countdownContainer.innerHTML = `${tiempo.diasRestantes} ${tiempo.horasRestantes} ${tiempo.minutosRestantes} ${tiempo.segundosRestantes}`;
+countdownContainer.style.color = "white";
+
+if(tiempo.tiempoRestante <= 1){
+    clearInterval(timer);
+}
+},1000)    
+
+}
+
+
+showCountDown('Nov 05 2022 18:00:00 GMT-0300');
